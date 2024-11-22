f52558846be4af93f7cbe111b2d1de284317af08
yoom <fdgh134@naver.com>
2024-11-22 22:26:31 +0900

    js-5weeks-hw-answer

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ function checkGuess(guess) {
       displayGuesses(guess);
       clearCircle();
       compareGuess(guess);
-    }  
-  }    
+    }
+  }
 }
 
 function validate(guess) {
@@ -53,12 +53,12 @@ function validate(guess) {
     alert("100 이하의 정수를 입력해 주세요");
     return false;
   }
-  
+
   return true;
 }
 
 function clearCircle() {
-    if ($guessCircleArea.childElementCount >= 1) {
+  if ($guessCircleArea.childElementCount >= 1) {
     $guessCircleArea.removeChild($guessCircleArea.firstChild);
   }
 }
@@ -83,12 +83,13 @@ function displayGuesses(guess) {
   $guessSlot.innerHTML += `${guess}  `;
   numGuesses++;
   // 구현 1. 남아있는 숫자를 보여줄 수 있도록 아래의 = 이후를 작성해주세요!
-  $remainingCount.innerHTML = ;
+  $remainingCount.innerHTML = `${11 - numGuesses}`;
 }
 
 // 유저에게 띄울 메세지를 입력합니다.
 function displayMessage(message) {
   // 구현 2. 유저에게 메세지를 보여줄 수 있도록 아래의 영역을 구현해주세요
+  $guessingResult.innerHTML = `<h1>${message}</h1>`;
 }
 
 function endGame() {
@@ -102,17 +103,24 @@ function endGame() {
 }
 
 function makeAnswerCircle(guess) {
-  const CIRCLE_NAME = "answer"
+  const CIRCLE_NAME = "answer";
   // 구현3. 유저가 원의 크기로 정답을 유추하기 쉽도록 showCircle 함수를 이용해서 해당 부분을 구현해주세요,
   // showCircle 함수의 "작업"이 끝나면, 해당 div에 원의 이름을 입력해주세요
+  showCircle(guess, CIRCLE_NAME, $answerCircleArea).then((div) => {
+    div.id = "answerCircle";
+    div.append(CIRCLE_NAME);
+  });
 }
 
 function makeGuessCircle(guess) {
-  const CIRCLE_NAME = "guess"
+  const CIRCLE_NAME = "guess";
   // 구현3. 유저가 원의 크기로 정답을 유추하기 쉽도록 showCircle 함수를 이용해서 해당 부분을 구현해주세요,
   // showCircle 함수의 "작업"이 끝나면, 해당 div에 원의 이름을 입력해주세요
+  showCircle(guess, CIRCLE_NAME, $guessCircleArea).then((div) => {
+    div.id = "guessCircle";
+    div.append(CIRCLE_NAME);
+  });
 }
-
 
 // 원을 그려주는 함수입니다, css로 그려지는 속도를 애니메이션으로 구현되어있으며,
 // 원의 크기, 이름, 영역을 받아 해당 영역에 해당 크기로 인자로 받은 이름을 붙여 그려줍니다.
